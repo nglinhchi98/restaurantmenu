28c6dbfcb18826cc63072c77cffafd8d74a15bcc
Chi Nguyen <nglinhchi65@gmail.com>
2022-12-30 17:59:58 +0200

    update of the menu project

---
+++ setup/src/data.js
@@ -0,0 +1,84 @@
+const menu = [
+  {
+    id: 1,
+    title: 'coolest banh mi on the block',
+    category: 'breakfast',
+    price: 10.9,
+    img: './images/menu_banhmi.jpeg',
+    desc: `Freshly baked bread. Home-made pickles, cucumber, coriander [W] [Soy]
+    Options: Pork, chicken or tofu. (V)
+    Option: Spicy sauteed beef `,
+  },
+  {
+    id: 2,
+    title: 'pho fighters',
+    category: 'lunch & dinner',
+    price: 15,
+    img: './images/menu_pho.jpeg',
+    desc: `Traditional Vietnamese Phở soup with fresh herbs and rice noodles
+    Beef: Rare beef, beef broth.
+    Chicken: tender chicken breast, beef broth.
+    Vegan: button mushrooms, broccoli, baby corn, tofu, veggie broth [VG] [Soy].`,
+  },
+  {
+    id: 3,
+    title: 'SAIGON beer, Vietnam, Lager',
+    category: 'drinks',
+    price: 7,
+    img: './images/menu_biasaigon.jpeg',
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: 'summer rolls breakpack',
+    category: 'breakfast',
+    price: 8.9,
+    img: './images/menu_summerroll.jpeg',
+    desc: `2 freshly made large rice paper rolls with veggies, mint, bell pepper [G*] [Soy]
+    Options: Chicken, shrimp, tofu [VG]. `,
+  },
+  {
+    id: 5,
+    title: 'poke bowl',
+    category: 'lunch & dinner',
+    price: 16.9,
+    img: './images/menu_pokebowl.jpeg',
+    desc: `Vietnamese-style poke bowl features hand-selected ingredients served over rice, greens, or rice & greens. `,
+  },
+  {
+    id: 6,
+    title: 'housemade cocktails',
+    category: 'drinks',
+    price: 13.9,
+    img: './images/menu_cocktail.jpeg',
+    desc: `Gintonic, Black Russian, Saigon Mule, Berries Collins. Ask staff for more options.`,
+  },
+  {
+    id: 7,
+    title: 'noodle around',
+    category: 'lunch & dinner',
+    price: 15,
+    img: './images/menu_buntron.jpeg',
+    desc: `Warm bowl of filling Viet noodles. Served with fresh greens, home-made pickles, crushed peanuts and fried onions
+    Options: Pork [Soy], Chicken [G*], Tofu [VG] [Soy].`,
+  },
+  {
+    id: 8,
+    title: 'dumplings darling',
+    category: 'lunch & dinner',
+    price: 13.9,
+    img: './images/menu_dumpling.jpeg',
+    desc: `8 handmade tender dumplings topped with coriander, fried onions, pickled onions, sesame seeds and served with soy sauce.
+    Options: Mixed of pork & chicken or vegan (Vg).
+    Extra rice & greens (Jasmin and red rice) + €2`,
+  },
+  {
+    id: 9,
+    title: 'wine of choice',
+    category: 'drinks',
+    price: '10 - 50',
+    img: './images/menu_wine.jpeg',
+    desc: `Please ask our staff for the options of wine. We have red, white, and sparkling wine at your service.`,
+  },
+];
+export default menu;
